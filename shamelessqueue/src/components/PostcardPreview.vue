7fbb9a68e5f316921638be77e2d57a3c2f1331bd
Marco Koch <marco@5th.ch>
2019-07-20 10:43:06 +0100

    include attachments via get img urls.

---
+++ shamelessqueue/src/components/PostcardPreview.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="postcard">
+    <img v-bind:src="getImgUrl()" alt="">
+    <p> {{  }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'PostcardPreview',
+    props: ['postcard'],
+    mounted() {
+    },
+    methods: {
+        /*getAttachment() {
+            this.$pouch.getAttachment(this.postcard._id, '922-536x354.jpg').then((response) => { this.postcard.img = response })
+        }*/
+        getImgUrl() {
+            return 'http://127.0.0.1:5984/postcards/' + this.postcard._id + '/' + Object.keys(this.postcard._attachments)[0]
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
--- shamelessqueue/src/components/Queue.vue
+++ shamelessqueue/src/components/Queue.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="hello">
     <ul v-for="postcard in postcards">
-        <li>{{ postcard }}ii</li>
+        <li>
+            <PostcardPreview v-bind:postcard="postcard"/>
+        </li>
     </ul>
   </div>
 </template>
 
 <script>
+import PostcardPreview from './PostcardPreview.vue'
+
 export default {
 	name: 'Queue',
+    data() {
+        return {
+            newPostcard: false
+        }
+    },
     pouch: {
         postcards: {}
     },
     created() {
         this.$pouch.sync('postcards', 'http://127.0.0.1:5984/postcards')
     }, 
+    components: {
+        PostcardPreview
+    }
 }
 </script>
 
