5001bfb3611eda9cc6bf8ba7238dbe2ec9aadf43
Marco Koch <marco@5th.ch>
2019-07-21 20:48:52 +0100

    döckering the sending of a postcard

---
+++ send_postcard/README.md
@@ -0,0 +1,10 @@
+# build
+```
+docker build -t sendpostcard  .
+```
+
+
+# run
+```
+docker run --env-file secret.txt sendpostcard
+```
--- send_postcard/test_send_postcard.py
+++ send_postcard/test_send_postcard.py
@@ -1,10 +1,15 @@
 from postcard_creator.postcard_creator import PostcardCreator, Postcard, Token, Recipient, Sender
 
+import os
+
 token = Token()
-token.fetch_token(username='', password='')
-token.has_valid_credentials(username='marco@5th.ch', password='ythTt96rjEzuWqD')
+token.fetch_token(username=os.environ['POSTUSER'], password=os.environ['POSTPASSWORD'])
+token.has_valid_credentials(username=os.environ['POSTUSER'], password=os.environ['POSTPASSWORD'])
+
 recipient = Recipient(prename='Marco', lastname='Koch', street='Niederwies 18', place='Vorderthal', zip_code=8857)
+
 sender = Sender(prename='Marco', lastname='Koch', street='Niederwies 18', place='Vorderthal', zip_code=8857)
+
 card = Postcard(message='test', recipient=recipient, sender=sender, picture_stream=open('./my-photo.jpg', 'rb'))
 
 w = PostcardCreator(token)
